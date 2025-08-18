2553315c65c03bbbf14db613f32a519282564c59
Jik932 <grills-zuurstof.3o@icloud.com>
2025-08-18 13:53:44 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', e => {
   self.skipWaiting(); // neem meteen over
   e.waitUntil(
-    caches.open('jigsaw-cache-v8').then(c => c.addAll([
+    caches.open('jigsaw-cache-v10').then(c => c.addAll([
       './',
       './index.html',
       './manifest.webmanifest',
