ac0cae355128d7af0fbd5d08edc7741026ba93d0
Jik932 <grills-zuurstof.3o@icloud.com>
2025-08-20 10:49:29 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump dit versienummer bij elke release
-const CACHE = 'jigsaw-cache-v22';
+const CACHE = 'jigsaw-cache-v23';
 
 self.addEventListener('install', e => {
   self.skipWaiting(); // neem meteen over
